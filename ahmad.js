1338b93a82a3e3f709f50375f399db1d2e1a583d
Ahmad Anwer Faiq Saleh <99225761+Ahmad6121@users.noreply.github.com>
2024-12-10 18:35:02 +0300

    Add files via upload

---
+++ ahmad.js
@@ -0,0 +1,33 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+// Get the carousel element
+const carousel = document.getElementById('demo');
+
+// Add event listener for mouse wheel
+carousel.addEventListener('wheel', (event) => {
+  // Prevent default scrolling behavior
+  event.preventDefault();
+
+  // Determine the scroll direction
+  if (event.deltaY > 0) {
+    // Scroll down, move to the next slide
+    bootstrap.Carousel.getInstance(carousel).next();
+  } else {
+    // Scroll up, move to the previous slide
+    bootstrap.Carousel.getInstance(carousel).prev();
+  }
+});
